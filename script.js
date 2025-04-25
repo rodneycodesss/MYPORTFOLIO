18d6e9e5a8d846b36fe30998e92fd793698f90df
Rodney <rodneygilbert321@gmail.com>
2025-04-25 12:48:36 +0300

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let lastScrollTop = 0;
 const header = document.querySelector("header");
-const timeText = document.querySelector(".time")
+const timeText = document.querySelector(".time");
 
 window.addEventListener("scroll", () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -15,9 +15,9 @@ window.addEventListener("scroll", () => {
 });
 
 const now = new Date();
-const jakartaTime = now.toLocaleString("en-US", { timeZone: "Asia/Jakarta" });
-const [date, time] = jakartaTime.split(", ");
-timeText.textContent = `${time} GMT+7`
+const nairobiTime = now.toLocaleString("en-US", { timeZone: "Africa/Nairobi" });
+const [date, time] = nairobiTime.split(", ");
+timeText.textContent = `${time} EAT`;
 
 const yearText = document.getElementById("year");
 const year = new Date().getFullYear();
